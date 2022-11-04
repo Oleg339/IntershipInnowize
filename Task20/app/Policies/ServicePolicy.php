f9b14727adf92dce0ee961818b6bb8c46d321429
Oleg339 <baharoleg224@gmail.com>
2022-11-04 16:38:09 +0300

    removed useless comments

--- Task20/app/Policies/ServicePolicy.php
+++ Task20/app/Policies/ServicePolicy.php
@@ -10,88 +10,36 @@ class ServicePolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param \App\Models\User $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny()
-    {
-        return auth()->user()->hasRole('Admin');
-    }
-
     public function edit()
     {
         return auth()->user()->hasRole('Admin');
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param \App\Models\User $user
-     * @param \App\Models\Service $service
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function view()
     {
         return auth()->user()->hasRole('Admin');
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param \App\Models\User $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function create()
     {
         return auth()->user()->hasRole('Admin');
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param \App\Models\User $user
-     * @param \App\Models\Service $service
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function update()
     {
         return auth()->user()->hasRole('Admin');
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param \App\Models\User $user
-     * @param \App\Models\Service $service
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function delete()
     {
         return auth()->user()->hasRole('Admin');
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param \App\Models\User $user
-     * @param \App\Models\Service $service
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function restore()
     {
         return auth()->user()->hasRole('Admin');
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param \App\Models\User $user
-     * @param \App\Models\Service $service
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function forceDelete()
     {
         return auth()->user()->hasRole('Admin');
